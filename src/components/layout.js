76d61b35127734f7b78217465f26c093002d7d3b
sadness_ojisan <sadness.ojisan@gmail.com>
2019-03-02 03:15:07 +0900

    update: show image

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import { Image, Button } from "re-geo"
 
 import Header from "./header"
-import "./layout.css"
+import "../util/reset.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -25,19 +26,13 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
+        <Button>aaaaaaaaaaa</Button>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div>
+          <img src={Image.Emailme} />
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
-            {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
         </div>
